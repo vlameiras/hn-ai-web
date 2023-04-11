18ab7d3add2a9a193619b02746b14d71b43c2c32
Vasco Lameiras <lameiras@gmail.com>
2023-04-11 23:47:17 +0100

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import { Analytics } from '@vercel/analytics/react';
 import { ReCaptchaProvider } from "next-recaptcha-v3";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <ReCaptchaProvider><Component {...pageProps} /></ReCaptchaProvider>
+  return <ReCaptchaProvider><Component {...pageProps} /><
+    Analytics
+  /></ReCaptchaProvider>
 }
