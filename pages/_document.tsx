d63fcbfb95371cec70d547fee0ee368f5898af49
Vasco Lameiras <lameiras@gmail.com>
2023-04-11 23:45:15 +0100

    add vercel analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,6 @@ export default function Document() {
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
         <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-        <script defer src="/_vercel/insights/script.js"></script>
     </Head>
       <body>
         <Main />
