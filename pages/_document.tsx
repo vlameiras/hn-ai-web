ae4d7d8ea6474ac64060b3ddbcc3d8fe31ea9422
Vasco Lameiras <lameiras@gmail.com>
2023-04-11 23:27:29 +0100

    initial commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+        <script defer src="/_vercel/insights/script.js"></script>
+    </Head>
       <body>
         <Main />
         <NextScript />
